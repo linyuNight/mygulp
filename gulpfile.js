425ee59bfb401de4ac41567a98330bc326e0e238
qq526645879 <526645879@qq.com>
2017-05-12 18:02:12 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var connect = require('gulp-connect');
+var spritesmith=require('gulp.spritesmith');
 
 var Asset = {
 	css:'scss/**/*.scss',
-	all:'client/**/*.*'
+	all:'client/**/*.*',
+	sprite:'client/image/*.png'
 }
 
 gulp.task('sass',function(){
@@ -15,6 +17,13 @@ gulp.task('sass',function(){
 		.pipe(connect.reload());
 })
 
+gulp.task('autoSprite', function () {
+  gulp.src(Asset.sprite).pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css',
+    padding: 20 
+  })).pipe(gulp.dest('client/image'));
+});
 
 gulp.task('toreload',function(){
 	gulp.src(Asset.all)
@@ -23,6 +32,7 @@ gulp.task('toreload',function(){
 
 gulp.task('watch',function(){
 	gulp.watch(Asset.css,['sass']);
+	gulp.watch(Asset.sprite,['autoSprite']);
 	gulp.watch(Asset.all,['toreload']);
 })
 
@@ -33,6 +43,6 @@ gulp.task('connect',function(){
 	});
 })
 
-gulp.task('default',['sass','watch','connect'],function() {
+gulp.task('default',['sass','watch','connect','autoSprite'],function() {
   	
 });
